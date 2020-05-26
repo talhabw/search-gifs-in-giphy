dcf234035fea95c68aa75fa31fea1bc2da8afc07
Talha Karasu <talhafb1223@gmail.com>
2020-05-26 22:12:40 +0300

    v1.0.0 // stable

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <search @searchRequested="handleSearch"></search>
+        <div class="container text-center mt-5 pt-5">
+            <h1 v-if="isLoading" class="text-info">LOADING...</h1>
+            <h1
+                v-else-if="!gifs.length"
+                class="text-danger"
+            >I couldn't find any gif with your keyword :(</h1>
+        </div>
+        <preview :gifs="gifs"></preview>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+let apikey = "YOUR_API_KEY";
+import Search from "./components/search";
+import Preview from "./components/preview";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "app",
+    components: {
+        Search,
+        Preview
+    },
+    data() {
+        return {
+            isLoading: true,
+            gifs: []
+        };
+    },
+    methods: {
+        doQuery(url) {
+            fetch(url)
+                .then(res => {
+                    return res.json();
+                })
+                .then(res => {
+                    this.gifs = res.data;
+                    this.isLoading = false;
+                });
+        },
+        handleSearch(query) {
+            if (query != "") {
+                this.gifs = [];
+                this.isLoading = true;
+                let searchUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${query}`;
+                this.doQuery(searchUrl);
+            } else if (query == "") {
+                let allUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${apikey}`;
+                this.doQuery(allUrl);
+            }
+        }
+    },
+    created() {
+        let url = `https://api.giphy.com/v1/gifs/trending?api_key=${apikey}`;
+        this.doQuery(url);
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 </style>
---
+++ src/components/preview.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+        <li v-for="gif in gifs" v-bind:key="gif">
+            <img :src="gif.images.fixed_height.url" alt />
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ["gifs"]
+};
+</script>
+
+<style>
+li {
+    list-style: none;
+    float: left;
+    margin: 20px;
+}
+</style>
